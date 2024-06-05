f300cabb198a812f798eaeae2530926e0dc7a31c
galennolan <galen.nolan1@gmail.com>
2024-06-05 14:59:19 +0700

    penjelasan user info

--- lib/helpers/user.info.dart
+++ lib/helpers/user.info.dart
@@ -1,31 +1,42 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-const String TOKEN = "token";
-const String USERNAME = "username";
+const String TOKEN = "token"; // Konstanta untuk kunci token
+const String USERNAME = "username"; // Konstanta untuk kunci nama pengguna
 
 class UserInfo {
+  // Metode untuk menyimpan token ke shared preferences
   Future<void> setToken(String value) async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    await pref.setString(TOKEN, value);
+    final SharedPreferences pref = await SharedPreferences
+        .getInstance(); // Mendapatkan instance shared preferences
+    await pref.setString(TOKEN, value); // Menyimpan token sebagai string
   }
 
+  // Metode untuk mengambil token dari shared preferences
   Future<String?> getToken() async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    return pref.getString(TOKEN);
+    final SharedPreferences pref = await SharedPreferences
+        .getInstance(); // Mendapatkan instance shared preferences
+    return pref.getString(TOKEN); // Mengambil token yang disimpan
   }
 
+  // Metode untuk menyimpan nama pengguna ke shared preferences
   Future<void> setUsername(String value) async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    await pref.setString(USERNAME, value);
+    final SharedPreferences pref = await SharedPreferences
+        .getInstance(); // Mendapatkan instance shared preferences
+    await pref.setString(
+        USERNAME, value); // Menyimpan nama pengguna sebagai string
   }
 
+  // Metode untuk mengambil nama pengguna dari shared preferences
   Future<String?> getUsername() async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    return pref.getString(USERNAME);
+    final SharedPreferences pref = await SharedPreferences
+        .getInstance(); // Mendapatkan instance shared preferences
+    return pref.getString(USERNAME); // Mengambil nama pengguna yang disimpan
   }
 
+  // Metode untuk menghapus semua data dari shared preferences (logout)
   Future<void> logout() async {
-    final SharedPreferences pref = await SharedPreferences.getInstance();
-    await pref.clear();
+    final SharedPreferences pref = await SharedPreferences
+        .getInstance(); // Mendapatkan instance shared preferences
+    await pref.clear(); // Menghapus semua data yang disimpan
   }
 }
